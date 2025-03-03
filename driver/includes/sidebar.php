da53b14b43240bdda58af7992037802da2d2accf
Md. Rashedul Islam Uzzal <rashed011191@gmail.com>
2025-03-03 08:46:12 +0600

    Add files via upload

---
+++ driver/includes/sidebar.php
@@ -0,0 +1,40 @@
+<div class="left_sidebar">
+        <nav class="sidebar">
+            <div class="user-info">
+                <div class="image"><a href="javascript:void(0);"><img src="../assets/images/user.png" alt="User"></a></div>
+                <div class="detail mt-3">
+                    <?php
+                     $did=$_SESSION['vamsdid'];
+                    $sql="SELECT Name,Email from  tbldriver where DriverID=:did";
+$query = $dbh -> prepare($sql);
+$query->bindParam(':did',$did,PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+
+foreach($results as $row)
+{    
+$email=$row->Email;   
+$fname=$row->Name;     
+}   ?>
+                    <h5 class="mb-0"><?php  echo $fname ;?></h5>
+                    <small><?php  echo $email ;?></small>
+                </div>
+            </div>
+            <ul id="main-menu" class="metismenu">
+            
+                <li class="active"><a href="dashboard.php"><i class="ti-home"></i><span>Dashboard</span></a></li>
+               
+               
+                <li>
+                    <a href="javascript:void(0)" class="has-arrow"><i class="ti-pencil-alt"></i><span>Assign Request</span></a>
+                    <ul>
+                        <li><a href="new-request.php">New Request</a></li>
+                        <li><a href="ontheway.php">On the way Request</a></li>
+                        <li><a href="completed.php">Completed Request</a></li>
+                        <li><a href="total-request.php">Total Request</a></li>
+                       
+                    </ul>
+                </li>
+            </ul>            
+        </nav>
+    </div>
